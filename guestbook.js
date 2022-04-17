23c6d43c4d3a53a4f5ba66486780dd047bc64860
nonmean <nonmean@gmail.com>
2022-04-17 18:55:58 +0000

    update apiUrl

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://de28551d.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
